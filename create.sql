558fa53ce501a96cb99af9972098980125eab3c5
Daisuke Kataoka <kataoka@h40.179.229.10.1017421.vlan.kuins.net>
2017-10-19 11:18:50 +0900

    Initial Commit

---
+++ create.sql
@@ -0,0 +1,48 @@
+CREATE TABLE IF NOT EXISTS query(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  q_id integer,
+  content_query text,ra
+  context_query text
+);
+
+CREATE TABLE IF NOT EXISTS tweet(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  tweet_id integer,
+  user_id integer,
+  favourites_count integer,
+  gained_at date,
+  created_at date,
+  body text,
+  location text
+);
+
+CREATE TABLE IF NOT EXISTS user(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  user_id integer,
+  gained_at date,
+  name text,
+  screen_name text,
+  profile text,
+  followers_count integer,
+  friends_count integer
+);
+
+CREATE TABLE IF NOT EXISTS json(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  url text,
+  json text,
+  api_type text
+);
+
+CREATE TABLE IF NOT EXISTS follower(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  user_id integer,
+  follower_id integer
+);
+
+CREATE TABLE IF NOT EXISTS qrels(
+  id integer PRIMARY KEY AUTOINCREMENT,
+  q_id integer,
+  tweet_id integer,
+  rank integer
+);
